b32fae3576d658a4a96d23bfff23a2839b8ea72d
Arunesh Naha <aruneshnaha18@gmail.com>
2025-06-10 08:09:47 +0530

    Adding Product image to the frontend

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom'
 function Card(props) {
   return (
     <div className="card m-2" style={{ width: "18rem" }}>
+      <img src={`http://localhost:8080/api/product/image/${props.productID}`}
+        style={{ width: "100%", height: "250px", objectFit: "cover", aspectRatio: "1 / 1" }}
+        className="card-img-top" alt="Product preview"></img>
       <div className="card-body">
         <h5 className="card-title">{props.productName}</h5>
         <h6 className="card-subtitle mb-2 text-body-secondary">{props.brand}</h6>
--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -32,14 +32,26 @@ export const ProductDetails = () => {
     <div className="container p-5 justify-content-center align-items-center">
       <strong><h1 className='text-center'>Product Details</h1></strong>
       <strong><h2 className='text-center'>Category: {product.category}</h2></strong>
-      <div className="card justify-content-center align-items-center" >
-        <div className="card-body">
-          <h3 className="card-title">{product.name}</h3>
-          <h4 className="card-subtitle mb-2 text-body-secondary">{product.brand}</h4>
-          <p className="card-text">{product.description}</p>
-          <strong>Rs. {product.price}</strong>
-          <br />
-          <p className='card-text'>Released date: {formatDate(product.releaseDate)}</p>
+      <div className="card justify-content-center align-items-center">
+        <div className="row g-0 align-items-center">
+          <div className="col-md-4 d-flex justify-content-center">
+            <img
+              src={`http://localhost:8080/api/product/image/${product.id}`}
+              alt="Product"
+              style={{ width: "250px", height: "250px", objectFit: "cover", aspectRatio: "1 / 1" }}
+              className="img-fluid rounded-start"
+            />
+          </div>
+          <div className="col-md-8">
+            <div className="card-body">
+              <h3 className="card-title">{product.name}</h3>
+              <h4 className="card-subtitle mb-2 text-body-secondary">{product.brand}</h4>
+              <p className="card-text">{product.description}</p>
+              <strong>Rs. {product.price}</strong>
+              <br />
+              <p className='card-text'>Released date: {formatDate(product.releaseDate)}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
